3d68929e10578fae43f2fe53ae57bb0c63f9e043
JaredzG <82556918+JaredzG@users.noreply.github.com>
2023-09-19 10:51:48 -0500

    remove url from allowed_domains

--- scraping/dota/dota/spiders/hero.py
+++ scraping/dota/dota/spiders/hero.py
@@ -3,7 +3,7 @@ from dota.items import HeroItem
 
 class HeroSpider(scrapy.Spider):
   name = "hero"
-  allowed_domains = ["https://dota2.fandom.com", "dota2.fandom.com"]
+  allowed_domains = ["dota2.fandom.com"]
   
   def start_requests(self):
     url = "https://dota2.fandom.com/wiki/Heroes"
@@ -13,7 +13,7 @@ class HeroSpider(scrapy.Spider):
     for hero in response.xpath('//table/tbody/tr/td/div/div/a'):
       hero_item = HeroItem()
       hero_item['name'] = hero.xpath('./@title').get()
-      yield response.follow(f'{self.allowed_domains[0]}{hero.xpath("./@href").get()}', callback=self.get_hero_data, meta={'hero_item': hero_item})
+      yield response.follow(f'https://dota2.fandom.com{hero.xpath("./@href").get()}', callback=self.get_hero_data, meta={'hero_item': hero_item})
       
   def get_hero_data(self, response):
     hero_item = response.meta['hero_item']
