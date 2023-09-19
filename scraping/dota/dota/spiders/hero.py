4c0dfca7de39a00b02c2549ab8ea69fa0ed3881a
JaredzG <82556918+JaredzG@users.noreply.github.com>
2023-09-18 23:53:52 -0500

    scrape hero names

--- scraping/dota/dota/spiders/hero.py
+++ scraping/dota/dota/spiders/hero.py
@@ -1,10 +1,20 @@
 import scrapy
-
+from dota.items import HeroItem
 
 class HeroSpider(scrapy.Spider):
-    name = "hero"
-    allowed_domains = ["dota2.fandom.com"]
-    start_urls = ["https://dota2.fandom.com/wiki/Heroes"]
+  name = "hero"
+  allowed_domains = ["https://dota2.fandom.com", "dota2.fandom.com"]
+  
+  def start_requests(self):
+    url = "https://dota2.fandom.com/wiki/Heroes"
+    yield scrapy.Request(url)
 
-    def parse(self, response):
-        pass
+  def parse(self, response):
+    for hero in response.xpath('//table/tbody/tr/td/div/div/a'):
+      hero_item = HeroItem()
+      hero_item['name'] = hero.xpath('./@title').get()
+      yield response.follow(f'{self.allowed_domains[0]}{hero.xpath("./@href").get()}', callback=self.get_hero_data, meta={'hero_item': hero_item})
+      
+  def get_hero_data(self, response):
+    hero_item = response.meta['hero_item']
+    yield hero_item
