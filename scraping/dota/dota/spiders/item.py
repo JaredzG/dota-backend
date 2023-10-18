3890070e416d5b5ea763f802d281b3e22be096e4
JaredzG <82556918+JaredzG@users.noreply.github.com>
2023-10-18 07:34:02 -0500

    update purchasable item types and categories

--- scraping/dota/dota/spiders/item.py
+++ scraping/dota/dota/spiders/item.py
@@ -24,14 +24,16 @@ class ItemSpider(scrapy.Spider):
         match url:
             case "https://dota2.fandom.com/wiki/Items":
                 item = ItemItem()
-                type = 'Purchasable'
-                categories = response.xpath('//h3[position()!=12 and position()<15]/span/@id').getall()
-                item_lists = response.xpath('//div[@class="itemlist"][position()<14]')
-                for i in range(len(item_lists)):
+                type = "Purchasable"
+                categories = self.get_item_categories(response)
+                category_item_lists = response.xpath('//div[@class="itemlist"][position()<14]')
+                for i in range(len(category_item_lists)):
                     classification = categories[i]
-                    item_list_items = item_lists[i].xpath('./div/a[position() mod 2 = 0]/@href').getall()
-                    for j in range(len(item_list_items)):
-                        item_url = "https://dota2.fandom.com" + item_list_items[j]
+                    if classification == "Boss Rewards" or classification == "Collectibles":
+                        type = "Non-Purchasable"
+                    category_items = category_item_lists[i].xpath('./div/a[position() mod 2 = 0]/@href').getall()
+                    for j in range(len(category_items)):
+                        item_url = "https://dota2.fandom.com" + category_items[j]
                         item_meta = {
                             "item": item,
                             "type": type,
@@ -40,14 +42,14 @@ class ItemSpider(scrapy.Spider):
                         yield response.follow(item_url, callback=self.get_item_data, meta=item_meta)
             case "https://dota2.fandom.com/wiki/Neutral_Items":
                 item = ItemItem()
-                type = 'Neutral'
+                type = "Neutral"
                 tiers = response.xpath('//h3[position()>1 and position()<7]/span/text()').getall()
-                item_lists = response.xpath('//div[@class="itemlist"][position()<6]')
-                for i in range(len(item_lists)):
+                tier_item_lists = response.xpath('//div[@class="itemlist"][position()<6]')
+                for i in range(len(tier_item_lists)):
                     classification = tiers[i]
-                    item_list_items = item_lists[i].xpath('./div/a[position() mod 2 = 0]/@href').getall()
-                    for j in range(len(item_list_items)):
-                        item_url = "https://dota2.fandom.com" + item_list_items[j]
+                    tier_items = tier_item_lists[i].xpath('./div/a[position() mod 2 = 0]/@href').getall()
+                    for j in range(len(tier_items)):
+                        item_url = "https://dota2.fandom.com" + tier_items[j]
                         item_meta = {
                             "item": item,
                             "type": type,
@@ -75,6 +77,17 @@ class ItemSpider(scrapy.Spider):
         return response.xpath('//span[@class="mw-page-title-main"]/text()').get()
     
     def get_item_stats(self, response):
-        stats = response.xpath('string(//table[@class="infobox"][1]//tr[th/span[contains(text(), "Bonus")]])').get().strip().split('+')[1:]
+        stats = response.xpath('string(//table[@class="infobox"][1]//tr[th/span[contains(text(), "Bonus")]])').get().strip().split("+")[1:]
         stats = stats if stats else "N/A"
         return stats
+    
+    def get_item_categories(self, response):
+        categories = response.xpath('//h3[position()!=12 and position()<15]/span/@id').getall()
+        for i in range(len(categories)):
+            category = categories[i]
+            if category == "Boss_Rewards":
+                category = "Boss Rewards"
+            elif category == "Collectible_Items":
+                category = "Collectibles"
+            categories[i] = category
+        return categories
