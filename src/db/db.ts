979bc5092c8ea32972ad9697c4e7706410065e4b
Jared Gallardo <jaredzgallardo@gmail.com>
2023-12-14 16:59:13 -0600

    add typing to db file

--- src/db/db.ts
+++ src/db/db.ts
@@ -4,11 +4,13 @@ import * as fs from "fs";
 
 const { Pool } = pg;
 
+const passwordFile: string | undefined = process.env.DOTA_DB_PASSWORD_FILE;
+
 export const pool = new Pool({
   host: process.env.DOTA_DB_HOST,
   port: 5432,
   user: process.env.DOTA_DB_USER,
-  password: fs.readFileSync(`${process.env.DOTA_DB_PASSWORD_FILE}`, "utf8"),
+  password: fs.readFileSync(`${passwordFile}`, "utf8"),
   database: process.env.DOTA_DB_NAME,
 });
 
@@ -19,9 +21,9 @@ export const createPool = () => {
     host: process.env.DOTA_DB_HOST,
     port: 5432,
     user: process.env.DOTA_DB_USER,
-    password: fs.readFileSync(`${process.env.DOTA_DB_PASSWORD_FILE}`, "utf8"),
+    password: fs.readFileSync(`${passwordFile}`, "utf8"),
     database: process.env.DOTA_DB_NAME,
   });
 };
 
-export const connectDB = (pool) => drizzle(pool);
+export const connectDB = (pool: pg.Pool) => drizzle(pool);
