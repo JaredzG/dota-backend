d82683382d93c0599d5f960fd08d156f5b127f84
Jared Gallardo <jaredzgallardo@gmail.com>
2023-12-23 23:04:19 -0600

    remove extra pool and database instance

--- src/db/db.ts
+++ src/db/db.ts
@@ -6,16 +6,6 @@ const { Pool } = pg;
 
 const passwordFile: string | undefined = process.env.DOTA_DB_PASSWORD_FILE;
 
-export const pool = new Pool({
-  host: process.env.DOTA_DB_HOST,
-  port: 5432,
-  user: process.env.DOTA_DB_USER,
-  password: fs.readFileSync(`${passwordFile}`, "utf8"),
-  database: process.env.DOTA_DB_NAME,
-});
-
-export const db = drizzle(pool);
-
 export const createPool = (): pg.Pool => {
   return new Pool({
     host: process.env.DOTA_DB_HOST,
