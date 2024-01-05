cd198c5179cfefede883fbbb9f068c377a628ea0
Jared Gallardo <jaredzgallardo@gmail.com>
2024-01-04 23:24:46 -0600

    update environment variable access

--- src/db/db.ts
+++ src/db/db.ts
@@ -5,15 +5,21 @@ import * as fs from "fs";
 
 const { Pool } = pg;
 
-const passwordFile: string | undefined = process.env.DOTA_DB_PASSWORD_FILE;
+const DB_HOST = process.env.DB_HOST ?? "postgres";
+const DB_PORT = parseInt(process.env.DB_PORT ?? "5432");
+const DB_USER = process.env.DB_USER ?? "postgres";
+const DB_PASSWORD_FILE =
+  process.env.DB_PASSWORD_FILE ?? "/run/secrets/postgres-password";
+const DB_PASSWORD = fs.readFileSync(DB_PASSWORD_FILE, "utf8");
+const DB_NAME = process.env.DB_NAME ?? "dota-database";
 
 export const createPool = async (): Promise<pg.Pool> => {
   return new Pool({
-    host: process.env.DOTA_DB_HOST,
-    port: 5432,
-    user: process.env.DOTA_DB_USER,
-    password: fs.readFileSync(`${passwordFile}`, "utf8"),
-    database: process.env.DOTA_DB_NAME,
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
   });
 };
 
