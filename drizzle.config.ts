89737d70b046a20fa26c6fc8e84dc498e0b21c5d
Jared Gallardo <jaredzgallardo@gmail.com>
2024-01-04 23:25:56 -0600

    update environment variable access

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,23 @@
 import type { Config } from "drizzle-kit";
 import * as fs from "fs";
 
+const DB_HOST = process.env.DB_HOST ?? "postgres";
+const DB_PORT = parseInt(process.env.DB_PORT ?? "5432");
+const DB_USER = process.env.DB_USER ?? "postgres";
+const DB_PASSWORD_FILE =
+  process.env.DB_PASSWORD_FILE ?? "/run/secrets/postgres-password";
+const DB_PASSWORD = fs.readFileSync(DB_PASSWORD_FILE, "utf8");
+const DB_NAME = process.env.DB_NAME ?? "dota-database";
+
 export default {
   schema: "./src/db/schema.ts",
   out: "./drizzle",
   driver: "pg",
   dbCredentials: {
-    host: process.env.DOTA_DB_HOST as string,
-    port: 5432,
-    user: process.env.DOTA_DB_USER,
-    password: fs.readFileSync("/run/secrets/postgres-password", "utf8"),
-    database: process.env.DOTA_DB_NAME as string,
+    host: DB_HOST,
+    port: DB_PORT,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
   },
 } satisfies Config;
